12d0da8365e2fd8350e34d23b8103e5c66f92591
staskachaev <staskachaev205@mail.ru>
2024-12-18 21:43:29 +0700

    create models book and author

--- library_app/models.py
+++ library_app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class Author(models.Model):
+    full_name = models.CharField(max_length=100, unique=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        ordering = ['full_name']
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    year_published = models.PositiveIntegerField(
+        validators=[
+            MinValueValidator(500),
+            MaxValueValidator(10000)
+        ]
+    )
+    genre = models.CharField(max_length=150)
+    category = models.CharField(max_length=150)
+    publisher_name = models.CharField(max_length=100)
+    cover = models.ImageField(upload_to='book-covers/', blank=True, null=True)
+    book_file = models.FileField(upload_to='books/', blank=True, null=True)
+
+    class Meta:
+        unique_together = ('title', 'author', 'year_published', 'publisher_name')
+
+    def __str__(self):
+        return f"{self.title} - {self.author.name}"
+
+
+
+
+
